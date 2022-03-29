e3f30c89813da03516a112603fe8af624cfe4417
Yurii Shchepilov <yuraschepilov@gmail.com>
2022-03-29 14:40:30 +0300

    used jsx to create element

--- src/index.js
+++ src/index.js
@@ -2,11 +2,48 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-const element = React.createElement('div', { a: 4, children: 'Hello' });
+const data = {
+  id: 'id-1',
+  url: 'https://cdn.pixabay.com/photo/2017/07/31/22/05/feathers-2561511_1280.jpg',
+  title: 'Feathers. Art abstract',
+  price: 500,
+  author: {
+    tag: 'ractapopulous',
+    url: 'https://pixabay.com/users/ractapopulous-24766/',
+  },
+  quantity: 10,
+};
 
-console.log('element', element);
+const painting = (
+  <div>
+    <img src={data.url} alt={data.title} width="480" />
+    <h2>{data.title}</h2>
+    <p>
+      Автоp: <a href={data.author.url}>{data.author.tag}</a>
+    </p>
+    <p>Цена: {data.price} кредитов</p>
+    <p>Доступность: заканчивается или есть в наличии</p>
+    <button type="button">Добавить в корзину</button>
+  </div>
+);
 
-ReactDOM.render(element, document.querySelector('#root'));
+ReactDOM.render(painting, document.querySelector('#root'));
+
+// const elem1 = <span>Hello</span>;
+// const elem2 = <span>World</span>;
+
+// // const element = React.createElement('div', { a: 4, children: 'Hello' });
+
+// const jsxElements = (
+//   <div>
+//     {elem1}
+//     {elem2}
+//   </div>
+// );
+
+// console.log(jsxElements);
+
+// ReactDOM.render(jsxElements, document.querySelector('#root'));
 
 // // If you want to start measuring performance in your app, pass a function
 // // to log results (for example: reportWebVitals(console.log))
