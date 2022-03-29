b3ad8ced0f2f1c562d67ec56acd9c14faf1fa1d9
Yurii Shchepilov <yuraschepilov@gmail.com>
2022-03-29 13:57:21 +0300

    used react and react-dom render

--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import './index.css';
-// import App from './App';
-// import reportWebVitals from './reportWebVitals';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
 
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// );
+const element = React.createElement('div', { a: 4, children: 'Hello' });
+
+console.log('element', element);
+
+ReactDOM.render(element, document.querySelector('#root'));
 
 // // If you want to start measuring performance in your app, pass a function
 // // to log results (for example: reportWebVitals(console.log))
